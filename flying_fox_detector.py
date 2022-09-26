6f4af777fc7f15331240f2fb0733ecfc7ce09963
Jonathan Roos <jonathanroos60@gmail.com>
2022-09-27 09:57:37 +1000

    added timers and changed somee code to improve performance

--- flying_fox_detector.py
+++ flying_fox_detector.py
@@ -1,13 +1,12 @@
+from timeit import default_timer as timer
+start = timer()
 import cv2 as cv
-import numpy as np
 from functions import  chop_img, find_bats
 from fastai.vision.all import *
+import pathlib 
 
-import pathlib
-
-
-img_nums = ("0115", "0690", "0217")
-img_num = img_nums[2]
+img_nums = ("0800", "0690", "0217")
+img_num = img_nums[0]
 img = cv.imread(r"C:\Users\jonathan\Evolve Technology\Evolve Technologies Team Site - Client Info\Ecosure\4. Projects\Project Flying Fox - Sample Data\PR5902 Hillview Station Apr 2022\Raw Data M2EA 270422\Ortho Runs\40M\Thermal\DJI_{}_T.JPG".format(img_num))
 
 # list of tuples that store each cropped image in its original format and threshed format (original, threshed)
@@ -16,7 +15,6 @@ allImgs = chop_img(img, 3)
 temp = pathlib.PosixPath
 pathlib.PosixPath = pathlib.WindowsPath
 learn = load_learner("model.pkl")
-
 # For each image, use cv.findcontours on threshed img and cv.drawcontours on original image
 allImgsBefore, allImgsAfter, totalBats = find_bats(allImgs, learn)
 
@@ -29,10 +27,11 @@ img_row_after_1 = cv.hconcat([allImgsAfter[0],allImgsAfter[1],allImgsAfter[2]])
 img_row_after_2 = cv.hconcat([allImgsAfter[3],allImgsAfter[4],allImgsAfter[5]])
 img_row_after_3 = cv.hconcat([allImgsAfter[6],allImgsAfter[7],allImgsAfter[8]])
 img_concat_after = cv.resize(cv.vconcat([img_row_after_1, img_row_after_2, img_row_after_3]), (960, 768))
+end = timer()
+print(end-start)
 cv.imshow("Before NN", img_concat_before)
 cv.imshow("After NN", img_concat_after)
 cv.waitKey(0)
 
 print(f"Number of bats: {totalBats}")
 
-               
--- functions.py
+++ functions.py
@@ -4,9 +4,7 @@ from fastai.vision.all import *
 
 def chop_img(img, step):
     allImgs = []
-    width = 640
-    height = 512
-    i, j = 0, 0
+    width, height, i, j = 640, 512, 0, 0
     for i in range(step):
         for j in range(step):
             # crop the image into step*rows and step*columns
@@ -29,16 +27,10 @@ def augment(img):
     return imgDilation
     
 def crop_bat(img, box):
-    x1, y1 = int(box[0][1]), int(box[0][0])
-    x2, y2 = int(box[1][1]), int(box[1][0]) 
-    x3, y3 = int(box[2][1]), int(box[2][0])
-    x4, y4 = int(box[3][1]), int(box[3][0])
+    x1, y1, x2, y2, x3, y3, x4, y4 = int(box[0][1]), int(box[0][0]), int(box[1][1]), int(box[1][0]), int(box[2][1]), int(box[2][0]), int(box[3][1]), int(box[3][0])
 
     # Find distance from cx to top_left_x and cy to top_left_y to determine how many pixels the border around the cropped image should be
-    top_left_x = min([x1,x2,x3,x4])
-    top_left_y = min([y1,y2,y3,y4])
-    bot_right_x = max([x1,x2,x3,x4])
-    bot_right_y = max([y1,y2,y3,y4])
+    top_left_x, top_left_y, bot_right_x, bot_right_y = min([x1,x2,x3,x4]), min([y1,y2,y3,y4]), max([x1,x2,x3,x4]), max([y1,y2,y3,y4])
 
     crop_x1 = top_left_x - 10 
     if crop_x1 <= 0:
@@ -82,7 +74,7 @@ def find_bats(allImgs, learn):
                 cropped_bat = crop_bat(img[0], box)
                 label, _, probs = learn.predict(cropped_bat)
                 p=f"{probs[0]:.4f}"
-                if label == '!bat' and p > '0.75':
+                if label == '!bat' and p > '0.5':
                     pass
                 else:
                     cv.drawContours(after, [box], 0, (0,0,255),1)
