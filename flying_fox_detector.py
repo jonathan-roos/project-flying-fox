e439850f00fe0c10e8ea8ce3c0af2b5bc1159b94
Jonathan Roos <jonathanroos60@gmail.com>
2022-09-27 16:56:18 +1000

    compulsory commit

--- flying_fox_detector.py
+++ flying_fox_detector.py
@@ -1,31 +1,36 @@
+from timeit import default_timer as timer
 import cv2 as cv
-import numpy as np
-import keyboard
-from functions import  chop_img, find_bats   
+from functions import  chop_img, find_bats
+from fastai.vision.all import *
+import pathlib
+import timm 
 
-img_nums = ["0902", "0690", "0217"]
-img_num = img_nums[1]
+start = timer()
+img_nums = ("0800", "0690", "0217")
+img_num = img_nums[2]
 img = cv.imread(r"C:\Users\jonathan\Evolve Technology\Evolve Technologies Team Site - Client Info\Ecosure\4. Projects\Project Flying Fox - Sample Data\PR5902 Hillview Station Apr 2022\Raw Data M2EA 270422\Ortho Runs\40M\Thermal\DJI_{}_T.JPG".format(img_num))
-print(img.shape)   
+totalBats = 0
+
 # list of tuples that store each cropped image in its original format and threshed format (original, threshed)
 allImgs = chop_img(img, 3)
 
 # For each image, use cv.findcontours on threshed img and cv.drawcontours on original image
-allImgs, totalBats, cropped_bats = find_bats(allImgs)
-    
-# Concatonate the marked images back together
-img_row_1 = cv.hconcat([allImgs[0][0],allImgs[1][0],allImgs[2][0]])
-img_row_2 = cv.hconcat([allImgs[3][0],allImgs[4][0],allImgs[5][0]])
-img_row_3 = cv.hconcat([allImgs[6][0],allImgs[7][0],allImgs[8][0]])
-img_concat = cv.resize(cv.vconcat([img_row_1, img_row_2, img_row_3]), (960, 768))
+cropped_bats = find_bats(allImgs)
 
-# label_bats(cropped_bats)
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+learn = load_learner("model_convnext_small.pkl")
 
-text = "Bats detected: {}".format(totalBats)
-cv.putText(img_concat, text, (350, 750), cv.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 2)
+i = 0
+while i < len(cropped_bats):
+    label, _, probs = learn.predict(cropped_bats[i])
+    p=f"{probs[0]:.4f}"
+    if label == '!bat' and p > '0.5':
+        pass
+    else:
+        totalBats += 1
+    i += 1
 
-print("Total number of bats = {}".format(totalBats))
-cv.imshow("img", img_concat)
-cv.waitKey(0)
 
-               
+print(timer()-start)
+print(f"Number of bats: {totalBats}")
--- functions.py
+++ functions.py
@@ -1,13 +1,11 @@
 import cv2 as cv
 import numpy as np
-import keyboard
-
+import timm
+from fastai.vision.all import *
 
 def chop_img(img, step):
     allImgs = []
-    width = 640
-    height = 512
-    i, j = 0, 0
+    width, height, i, j = 640, 512, 0, 0
     for i in range(step):
         for j in range(step):
             # crop the image into step*rows and step*columns
@@ -29,17 +27,11 @@ def augment(img):
     imgDilation = cv.dilate(imgThresh, kernel, iterations=1)  # apply dilation to amplify threshold result
     return imgDilation
     
-def crop_bat(img, box, index):
-    x1, y1 = int(box[0][1]), int(box[0][0])
-    x2, y2 = int(box[1][1]), int(box[1][0]) 
-    x3, y3 = int(box[2][1]), int(box[2][0])
-    x4, y4 = int(box[3][1]), int(box[3][0])
+def crop_bat(img, box):
+    x1, y1, x2, y2, x3, y3, x4, y4 = int(box[0][1]), int(box[0][0]), int(box[1][1]), int(box[1][0]), int(box[2][1]), int(box[2][0]), int(box[3][1]), int(box[3][0])
 
     # Find distance from cx to top_left_x and cy to top_left_y to determine how many pixels the border around the cropped image should be
-    top_left_x = min([x1,x2,x3,x4])
-    top_left_y = min([y1,y2,y3,y4])
-    bot_right_x = max([x1,x2,x3,x4])
-    bot_right_y = max([y1,y2,y3,y4])
+    top_left_x, top_left_y, bot_right_x, bot_right_y = min([x1,x2,x3,x4]), min([y1,y2,y3,y4]), max([x1,x2,x3,x4]), max([y1,y2,y3,y4])
 
     crop_x1 = top_left_x - 10 
     if crop_x1 <= 0:
@@ -57,77 +49,24 @@ def crop_bat(img, box, index):
     if crop_y2 > 640:
         crop_y2 = 640 
     
-    print(top_left_x, top_left_y, bot_right_x, bot_right_y)
     bat_crop = img[crop_x1: crop_x2, crop_y1: crop_y2]
 
-    # bat_crop = img[top_left_x-10: bot_right_x+11, top_left_y-10: bot_right_y+11]
-    
-
-    label_bat(bat_crop, img, box, index)
-
     return bat_crop
-
     
 def find_bats(allImgs):
-    totalBats = 0
-    batDepthMin = 50
-    batDepthMax = 400
+    batDepthMin, batDepthMax = 50, 400
     cropped_bats = []
-    bats = []
 
     for img in allImgs:
-        blobs = cv.findContours(img[1], cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)[-2]  # Find bats using cv.findContours
+        blobs = cv.findContours(img[1], cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)[-2]
         # Process the blobs
-        i =0
         for blob in blobs:
-            if batDepthMin < cv.contourArea(blob) < batDepthMax:  # Only process blobs with a min / max size
+            if batDepthMin < cv.contourArea(blob) < batDepthMax:  # Only process blobs with a min / max size 
                 rect = cv.minAreaRect(blob)
                 box = cv.boxPoints(rect)
                 box = np.int0(box)
-                M = cv.moments(blob)
-                cx = int(M["m10"] / M["m00"])
-                cy = int(M["m01"] / M["m00"])
-                bat_cords = (cx, cy)
-                bats.append(bat_cords)
-                # crop bat from image
-                
-                cropped_bat = crop_bat(img[0], box, i)
-                cropped_bats.append((cropped_bat, bat_cords, box))
-                i+=1
+                cropped_bat = crop_bat(img[0], box)
+                cropped_bats.append(cropped_bat)
+                        
+    return cropped_bats
 
-    totalBats += len(bats)
-        
-    return allImgs, totalBats, cropped_bats
-
-def label_bat(bat_crop, img, box, i):
-    num = i 
-    # print(bat_crop.shape)
-    cv.imshow("cropped_bat", bat_crop)
-    img_dup = img.copy()
-    cv.drawContours(img_dup, [box], 0, (0, 0, 255), 1)  # Draw bat contours on img original
-    cv.imshow("context", img_dup)
-    
-    cv.waitKey(0)
-    
-    if keyboard.is_pressed('1'):
-        print("1 was pressed")
-        path = r"C:\Users\jonathan\OneDrive - Evolve Technology\Documents\Project Flying Fox\croppedBatsv2\1bat\bat{}.png".format(num)
-        cv.imwrite(path, bat_crop)
-
-    if keyboard.is_pressed('0'):
-        print("0 was pressed")
-        path = r"C:\Users\jonathan\OneDrive - Evolve Technology\Documents\Project Flying Fox\croppedBatsv2\!bat\bat{}.png".format(num)
-        cv.imwrite(path, bat_crop)
-
-    if keyboard.is_pressed('2'):
-        print("2 was pressed")
-        path = r"C:\Users\jonathan\OneDrive - Evolve Technology\Documents\Project Flying Fox\croppedBatsv2\2bats\bat{}.png".format(num)
-        cv.imwrite(path, bat_crop)
-    
-    if keyboard.is_pressed('3'):
-        print("3 was pressed")
-        path = r"C:\Users\jonathan\OneDrive - Evolve Technology\Documents\Project Flying Fox\croppedBatsv2\3orMoreBats\bat{}.png".format(num)
-        cv.imwrite(path, bat_crop)
-    
-    
-    cv.destroyAllWindows()
