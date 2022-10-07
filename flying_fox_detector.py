4951ec171cc020d5014d97774319d819d90c6309
Jonathan <jonathanroos60@gmail.com>
2022-10-07 14:50:49 +1000

    missed file in last commit

--- flying_fox_detector.py
+++ flying_fox_detector.py
@@ -1,13 +1,12 @@
 import cv2 as cv
 from functions import  chop_img, find_bats
-from fastai.vision.all import *
+from fastai.vision.all import load_learner
+from fastai.basics import default_device
 import pathlib
 import time
 
 start = time.time()
-img_nums = ("0043", "0690", "0217")
-img_num = img_nums[0]
-img = cv.imread(r"C:\Users\jonathan\Evolve Technology\Evolve Technologies Team Site - Client Info\Ecosure\4. Projects\Project Flying Fox - Sample Data\PR5902 Hillview Station Apr 2022\Raw Data M2EA 270422\Ortho Runs\40M\Thermal\DJI_{}_T.JPG".format(img_num))
+img = cv.imread(r"DJI_0017_T.JPG")
 totalBats = 0
 
 # list of tuples that store each cropped image in its original format and threshed format (original, threshed)
@@ -19,11 +18,18 @@ for img in allImgs:
 
 temp = pathlib.PosixPath
 pathlib.PosixPath = pathlib.WindowsPath
-learn = load_learner("model_densenet_zeros.pkl")
+
+# Checking if GPU is available
+# use_cuda = torch.cuda.is_available()
+# device = torch.device("cuda" if use_cuda else "cpu")
+# default_device(use_cuda=use_cuda)
+
+learn = load_learner("model.pkl", cpu=False)
 print(f"Time before Detecting bats took {time.time() - start:.2f} seconds")
 print("Detecting Bats...")
 
 start1 = time.time()
+
 
 def predict(bat):
     with learn.no_bar(), learn.no_logging():
@@ -36,5 +42,4 @@ filtered_results = filter(lambda x: (x[0] < '0.5' and x[1] > '0.75'), results)
 
 print(len(list(filtered_results)))
 print(f"Detecting Bats took: {time.time()-start1:.2f} seconds")
-
 
