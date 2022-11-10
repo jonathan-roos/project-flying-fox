2f4269e9574df05e94e2dd3357d8c5791dfbf7c8
Jonathan Roos <jonathanroos60@gmail.com>
2022-11-10 13:29:49 +1000

    batch predictions

--- flying_fox_detector.py
+++ flying_fox_detector.py
@@ -1,8 +1,9 @@
 import cv2 as cv
 from functions import  chop_img, find_bats
-from fastai.vision.all import load_learner
-from fastai.basics import default_device
+from fastai.vision.all import *
 import pathlib
+import torch
+import torch.nn 
 import time
 
 start = time.time()
@@ -19,27 +20,40 @@ for img in allImgs:
 temp = pathlib.PosixPath
 pathlib.PosixPath = pathlib.WindowsPath
 
-# Checking if GPU is available
-# use_cuda = torch.cuda.is_available()
-# device = torch.device("cuda" if use_cuda else "cpu")
-# default_device(use_cuda=use_cuda)
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# learn = torch.load('model.pkl')
+# learn = torch.nn.DataParallel(learn)
+# learn.to(device)
 
 learn = load_learner("model_densenet_zeros.pkl", cpu=True) # Change to cpu=False if using a GPU
-print(f"Time before Detecting bats took {time.time() - start:.2f} seconds")
-print("Detecting Bats...")
+if __name__ == '__main__':
+    test_dl = learn.dls.test_dl(cropped_bats[:50])
+    preds = learn.get_preds(dl=test_dl)
 
-start1 = time.time()
+    # print(preds)
+    # results = [tuple(f"{pred[0]:.4f}", f"{pred[1]:.4f}") for pred in preds]
+
+    results = [tuple([f"{pred[0]:.4f}",f"{pred[1]:.4f}"]) for pred in preds[0]]
+    for result in results:
+        print(result)
+
+# print(f"Time before Detecting bats took {time.time() - start:.2f} seconds")
+# print("Detecting Bats...")
+
+# start1 = time.time()
 
 
-def predict(bat):
-    with learn.no_bar(), learn.no_logging():
-        _, _, probs = learn.predict(bat)
-    return (tuple(map(lambda x: f"{x:.4f}", probs)))
+# def predict(bat):
+#     with learn.no_bar(), learn.no_logging():
+#         _, _, probs = learn.predict(bat)
+#     return (tuple(map(lambda x: f"{x:.4f}", probs)))
 
-results = [predict(bat) for bat in cropped_bats]
 
-filtered_results = filter(lambda x: (x[0] < '0.5' and x[1] > '0.75'), results)
 
-print(len(list(filtered_results)))
-print(f"Detecting Bats took: {time.time()-start1:.2f} seconds")
+# results = [predict(bat) for bat in cropped_bats]
+
+# filtered_results = filter(lambda x: (x[0] < '0.5' and x[1] > '0.75'), results)
+
+# print(len(list(filtered_results)))
+# print(f"Detecting Bats took: {time.time()-start1:.2f} seconds")
 
