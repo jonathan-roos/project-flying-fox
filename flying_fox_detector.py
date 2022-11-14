3636c7acc5019df83a80946ff3171d9541a2d773
Jonathan Roos <jonathanroos60@gmail.com>
2022-11-14 14:46:11 +1000

    Implemented batch predictions. Only returns the number of filtered bats. Does not have the functionality to display the contours. Is possible to implement though.

--- flying_fox_detector.py
+++ flying_fox_detector.py
@@ -8,7 +8,6 @@ import time
 
 start = time.time()
 img = cv.imread(r"DJI_0017_T.JPG")
-totalBats = 0
 
 # list of tuples that store each cropped image in its original format and threshed format (original, threshed)
 allImgs = chop_img(img, 3)
@@ -20,40 +19,19 @@ for img in allImgs:
 temp = pathlib.PosixPath
 pathlib.PosixPath = pathlib.WindowsPath
 
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# learn = torch.load('model.pkl')
-# learn = torch.nn.DataParallel(learn)
-# learn.to(device)
-
+start = time.time()
 learn = load_learner("model_densenet_zeros.pkl", cpu=True) # Change to cpu=False if using a GPU
+
+# getting batch predictions using learn.get_preds() instead of using learn.predict().
+# results in much quicker prediction times
 if __name__ == '__main__':
-    test_dl = learn.dls.test_dl(cropped_bats[:50])
+    test_dl = learn.dls.test_dl(cropped_bats)
     preds = learn.get_preds(dl=test_dl)
 
-    # print(preds)
-    # results = [tuple(f"{pred[0]:.4f}", f"{pred[1]:.4f}") for pred in preds]
-
     results = [tuple([f"{pred[0]:.4f}",f"{pred[1]:.4f}"]) for pred in preds[0]]
-    for result in results:
-        print(result)
 
-# print(f"Time before Detecting bats took {time.time() - start:.2f} seconds")
-# print("Detecting Bats...")
+    filtered_results = filter(lambda x: (x[0] < '0.5' and x[1] > '0.75'), results)
 
-# start1 = time.time()
-
-
-# def predict(bat):
-#     with learn.no_bar(), learn.no_logging():
-#         _, _, probs = learn.predict(bat)
-#     return (tuple(map(lambda x: f"{x:.4f}", probs)))
-
-
-
-# results = [predict(bat) for bat in cropped_bats]
-
-# filtered_results = filter(lambda x: (x[0] < '0.5' and x[1] > '0.75'), results)
-
-# print(len(list(filtered_results)))
-# print(f"Detecting Bats took: {time.time()-start1:.2f} seconds")
+    print(len(list(filtered_results)))
+    print(f"Detecting Bats took: {time.time()-start:.2f} seconds")
 
